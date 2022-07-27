b58c2f1159504043dd2656105d2d0e44530491b5
Fedor Volkov <f.volkov10@gmail.com>
2022-07-27 11:39:18 +0300

    input styled

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.querySelector(".main__button");
 const buttonIcon = document.querySelector(".main__button_icon");
 const chat = document.querySelector(".main__chat");
 let isChatHidden = true;
+const inputTextArea = document.querySelector(".input__textarea");
+const inputSend = document.querySelector(".input__send");
 
 setTimeout(() => {
   buttonIcon.classList.add("main__button_icon-chat");
@@ -21,4 +23,41 @@ button.addEventListener("click", () => {
   chat.classList.toggle("main__chat_hidden");
 
   isChatHidden = !isChatHidden;
+});
+
+inputTextArea.addEventListener("input", (ev) => {
+  if (inputTextArea.value.length > 0) {
+    inputSend.classList.remove("input__send_hidden");
+  } else {
+    inputSend.classList.add("input__send_hidden");
+  }
+
+  const inputTextArea1 = inputTextArea.cloneNode(true);
+  inputTextArea1.style.opacity = "0";
+  inputTextArea.after(inputTextArea1);
+  let diff = 0;
+  if (
+    inputTextArea1.scrollHeight > inputTextArea1.clientHeight &&
+    inputTextArea1.rows < 5
+  ) {
+    diff = 1;
+  } else if (inputTextArea1.rows > 1) {
+    while (
+      !(inputTextArea1.scrollHeight > inputTextArea1.clientHeight) &&
+      inputTextArea1.rows > 2
+    ) {
+      inputTextArea1.rows -= 1;
+      --diff;
+    }
+    if (inputTextArea1.rows === 2) {
+      inputTextArea1.rows -= 1;
+      --diff;
+      if (inputTextArea1.scrollHeight > inputTextArea1.clientHeight) {
+        ++diff;
+      }
+    }
+  }
+  
+  inputTextArea.rows += diff;
+  inputTextArea1.remove();
 });
